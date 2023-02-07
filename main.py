8ba682656ff3d8dc19717d8fa074637a84d26e22
mx79 <lesage.max@sfr.fr>
2023-02-07 09:51:58 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import logging
+import uvicorn
+
+from app.api import app
+
+
+if __name__ == '__main__':
+    logging.info("starting uvicorn web server")
+    uvicorn.run(app, host='0.0.0.0', port=int(os.getenv("PORT", 80)), log_level='info')
