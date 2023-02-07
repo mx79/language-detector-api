2a5b6f61f2d3a382ba88d9611e686555d51619dc
mx79 <lesage.max@sfr.fr>
2023-02-07 10:09:17 +0100

    initial commit

--- app/controllers/language.py
+++ app/controllers/language.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 
 
 def get_language(sentence: str) -> Dict[str, Any]:
-    """Returns the language for a given sentence..
+    """Returns the language for a given sentence.
 
     :param sentence: The given sentence
     :return: Res dict
